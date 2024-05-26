b65849ff4453461cccea2ea00baefb698537ff91
Muhammed İkbal <m.ikbal0302l@gmail.com>
2024-05-26 22:56:47 +0300

    Some bugs were fixed and confusion matrix evaluation was added.

--- Main.py
+++ Main.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
 
 from tensorflow.keras.layers import Input, Conv2D, BatchNormalization, Activation, Add, MaxPooling2D, \
     GlobalAveragePooling2D, Dense
@@ -10,7 +13,8 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import EarlyStopping
 
 
-def resnet_block(input_tensor, filters, kernel_size=3, strides=1, conv_shortcut=True):
+def resnet_block(input_tensor, filters, kernel_size=3, strides=1,conv_shortcut=True):
+
     x = Conv2D(filters, kernel_size, strides=strides, padding='same')(input_tensor)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
@@ -29,7 +33,7 @@ def resnet_block(input_tensor, filters, kernel_size=3, strides=1, conv_shortcut=
     return x
 
 
-def resnet152(input_shape=(64, 64, 3), num_classes=26):
+def resnet152(input_shape=(64, 64, 3), num_classes=10):
     inputs = Input(shape=input_shape)
 
     x = Conv2D(64, kernel_size=7, strides=2, padding='same')(inputs)
@@ -68,8 +72,8 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
 x_train = x_train.astype('float32') / 255.0
 x_test = x_test.astype('float32') / 255.0
-y_train = tf.keras.utils.to_categorical(y_train, num_classes=26)
-y_test = tf.keras.utils.to_categorical(y_test, num_classes=26)
+y_train = tf.keras.utils.to_categorical(y_train, num_classes=10)
+y_test = tf.keras.utils.to_categorical(y_test, num_classes=10)
 
 model = resnet152()
 
@@ -85,10 +89,21 @@ x_test = tf.image.resize(x_test, [64, 64])
 x_train = x_train.numpy()
 x_test = x_test.numpy()
 
+
 optimizer = Adam(learning_rate=0.001)
 
 early_stopping = EarlyStopping(monitor='val_loss', patience=5)
 
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(x_train, y_train, batch_size=32, epochs=10, validation_data=(x_test, y_test), callbacks=[early_stopping])
+model.fit(x_train, y_train, batch_size=32, epochs=50, validation_data=(x_test, y_test), callbacks=[early_stopping])
+
+y_pred = model.predict(x_test)
+y_pred_classes = np.argmax(y_pred, axis=1)
+y_true_classes = np.argmax(y_test, axis=1)
+cm = confusion_matrix(y_true_classes, y_pred_classes)
+plt.figure(figsize=(10, 8))
+sns.heatmap(cm, annot=True, fmt='d', cmap='Purples', xticklabels=digits.target_names, yticklabels=digits.target_names)
+plt.xlabel('Tahmin')
+plt.ylabel('Doğruluk')
+plt.show()
